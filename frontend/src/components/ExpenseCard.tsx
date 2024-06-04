951f32f453698dee2bd6d7b289d82c0d70332ee9
Ayush <ayushkumar9601@gmail.com>
2024-06-04 23:23:51 +0530

    edit expense

--- frontend/src/components/ExpenseCard.tsx
+++ frontend/src/components/ExpenseCard.tsx
@@ -1,14 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "./ui/badge";
+import { AddExpenseCard } from "./AddExpenseCard";
 
 export const ExpenseCard = () => {
     return (
         <div>
             <Card className="w-[300px]">
-                <CardHeader>
-                    <CardTitle>Food</CardTitle>
-                </CardHeader>
+                <AddExpenseCard title="Edit Expense">
+                    <CardHeader className="flex flex-row justify-between">
+                        <CardTitle>Food</CardTitle>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 cursor-pointer">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                        </svg>
+                    </CardHeader>
+                </AddExpenseCard>
 
                 <CardContent>
                     <CardDescription>This is description</CardDescription>
